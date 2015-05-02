8bfd472ee7410888e2d7cecd6ee912e2fbb5cadd
rigoxls <rigoxls@gmail.com>
2015-05-02 10:29:37 -0500

    new order fields

--- app/models/Order.php
+++ app/models/Order.php
@@ -4,5 +4,4 @@ class Order extends \Eloquent {
     protected $table = 'order';
 	protected $fillable = array("customer_id","usd_total","rate","delivery_address");
     protected $primaryKey = 'order_id';
-    public $timestamps = false;
 }
